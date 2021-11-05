390510c78dcb8a3dc039d6f8942fe8a1fce56e02
Cigaro <dimagilew@mail.ru>
2021-11-05 04:57:18 +0300

    feat: add copy directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,37 @@
+const fs = require('fs');
+const copyFrom = '04-copy-directory/files';
+const copyTo = '04-copy-directory/files-copy/';
 
+
+
+function copyFile(file, way){
+  console.log(file);
+  fs.copyFile(file, way, err => {
+    if(err) throw err; // не удалось скопировать файл
+    console.log('Файл успешно скопирован');
+  });
+}
+
+
+function mkDir(){
+  fs.mkdir('04-copy-directory/files-copy', err => {
+    if(err) {
+      if (err.errno === -4075){
+        console.log('hi');
+        return listObjects();
+      } else throw err;
+    } else  listObjects();
+  });
+}
+
+function listObjects(){
+  fs.readdir(copyFrom, (err, files) => {
+    if(err) throw err;
+    for(let i = 0; i< files.length; i++){
+      const a = '/';
+      let adrees = copyFrom + a +files[i];
+      let adreesTo = copyTo + a + files[i];
+      copyFile(adrees, adreesTo);
+    }
+  });}
+mkDir();
